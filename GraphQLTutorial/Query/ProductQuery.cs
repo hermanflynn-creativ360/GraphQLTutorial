4136b73443e973d41de40da8fb678ca2679e5000
EvilOnion <mhg2flynn@gmail.com>
2022-10-14 18:12:29 +0200

    GraphQL Queries

---
+++ GraphQLTutorial/Query/ProductQuery.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQLTutorial.Interfaces;
+using GraphQLTutorial.Type;
+
+namespace GraphQLTutorial.Query
+{
+    public class ProductQuery : ObjectGraphType
+    {
+        public ProductQuery(IProject productService) 
+        {
+            Field<ListGraphType<ProductType>>("products", resolve: contex=> { return productService.GetAllProducts(); });
+
+            Field<ProductType>("product", arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+                resolve: contex => { return productService.GetProductByID(contex.GetArgument<int>("id")); });
+        }
+    }
+}
