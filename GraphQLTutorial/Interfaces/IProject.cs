b0b4796258468819084eb96ab4b92ebc6743f9e8
EvilOnion <mhg2flynn@gmail.com>
2022-10-14 12:43:37 +0200

    Add project files.

---
+++ GraphQLTutorial/Interfaces/IProject.cs
@@ -0,0 +1,12 @@
+﻿using GraphQLTutorial.Models;
+
+namespace GraphQLTutorial.Interfaces
+{
+    public interface IProject
+    {
+        List<Product> GetAllProducts();
+        Product AddProject(Product product);
+        Product UpdateProject(int id, Product product);
+        void DeleteProject(int id);
+    }
+}
