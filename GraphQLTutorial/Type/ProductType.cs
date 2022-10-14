42ffbfe078f0928820f84a0e1ce32dd2f369c2f5
EvilOnion <mhg2flynn@gmail.com>
2022-10-14 18:00:38 +0200

    GraphQL Types

---
+++ GraphQLTutorial/Type/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQLTutorial.Models;
+
+namespace GraphQLTutorial.Type
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType() 
+        {
+            Field(x => x.Id);
+            Field(x => x.Name);
+            Field(x => x.Price);
+        }
+    }
+}
